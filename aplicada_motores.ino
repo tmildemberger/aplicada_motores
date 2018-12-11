c3dc2e894941fd43e289ebab61260207fea2965f
tmildemberger <thimil@alunos.utfpr.edu.br>
2018-12-11 10:13:50 -0200

    muito bom

--- aplicada_motores.ino
+++ aplicada_motores.ino
@@ -5,10 +5,10 @@
 #define PINO_MOTOR_EIXOX_STEP 6
 #define PINO_MOTOR_EIXOX_DIR 5
 #define MOTOR_EIXO_X_VELOCIDADE 100 /*passos por segundo*/
-#define MOTOR_EIXO_X_ACELERACAO 100 /*passos por segundo por segundo*/
+#define MOTOR_EIXO_X_ACELERACAO 120 /*passos por segundo por segundo*/
 
-#define PASSOS_PARA_O_PROXIMO_PONTO 10
-#define PASSOS_PARA_A_PROXIMA_LETRA 20
+#define PASSOS_PARA_O_PROXIMO_PONTO 18
+#define PASSOS_PARA_A_PROXIMA_LETRA 36
 /*Fim do motor eixo X*/
 
 /*Motor que controla a punção*/
@@ -66,7 +66,7 @@ void setup() {
     motor_papel1.run();
     motor_papel2.run();
     }*/
-
+/*
   while (digitalRead(PINO_SENSOR_DE_FIM_DE_CURSO) == HIGH) {
     if (!motor_eixo_x.isRunning()) {
       motor_eixo_x.move(-20000);
@@ -81,54 +81,88 @@ void setup() {
   motor_eixo_x.move(20);
   motor_eixo_x.runToPosition();
   motor_eixo_x.setCurrentPosition(0);
-  delay(1000);
+  delay(1000);*/
   /*considerando que o joão começa na horizontal*/
   motor_joao.move(100);
   motor_joao.runToPosition();
   motor_joao.move(550);
-  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 8);
+  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 4);
+  while (motor_joao.distanceToGo() > 400){
+    motor_joao.run();
+  }
 }
 
 void loop() {
   motor_joao.move(800);
-  while (motor_joao.distanceToGo() > 700){
-    motor_joao.run();
-  }
-  /*100 passos a velocidade 100*/
-  
-  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE);
-  motor_eixo_x.move(PASSOS_PARA_O_PROXIMO_PONTO);
-  while (motor_joao.distanceToGo() > 550){
-    motor_joao.run();
-    motor_eixo_x.run();
-  }
-  /*acelerando durante 150 passos*/
-  
-  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 8);
+  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 16);
   while (motor_joao.distanceToGo() > 400){
     motor_joao.run();
-    motor_eixo_x.run();
   }
-  /*desacelerando durante 150 passos*/
-  
+
   motor_joao.move(800);
-  while (motor_joao.distanceToGo() > 700){
-    motor_joao.run();
-  }
-  /*100 passos a velocidade 100*/
-  
-  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 4);
-  motor_eixo_x.move(PASSOS_PARA_A_PROXIMA_LETRA);
-  while (motor_joao.distanceToGo() > 550){
-    motor_joao.run();
-    motor_eixo_x.run();
-  }
-  /*acelerando durante 150 passos*/
-  
   motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 8);
   while (motor_joao.distanceToGo() > 400){
     motor_joao.run();
-    motor_eixo_x.run();
   }
-  /*desacelerando durante 150 passos*/
+
+  motor_joao.move(800);
+  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 4);
+  while (motor_joao.distanceToGo() > 400){
+    motor_joao.run();
+  }
+
+  motor_joao.move(800);
+  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 2);
+  while (motor_joao.distanceToGo() > 400){
+    motor_joao.run();
+  }
+
+  /*delay(1000);
+  
+  motor_eixo_x.move(PASSOS_PARA_O_PROXIMO_PONTO);
+  motor_eixo_x.runToPosition();*/
+
+  delay(500);
+  
+//  motor_joao.move(800);
+//  while (motor_joao.distanceToGo() > 700){
+//    motor_joao.run();
+//  }
+//  /*100 passos a velocidade 100*/
+//  
+//  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE);
+//  motor_eixo_x.move(PASSOS_PARA_O_PROXIMO_PONTO);
+//  while (motor_joao.distanceToGo() > 550){
+//    motor_joao.run();
+//    motor_eixo_x.run();
+//  }
+//  /*acelerando durante 150 passos*/
+//  
+//  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 4);
+//  while (motor_joao.distanceToGo() > 400){
+//    motor_joao.run();
+//    motor_eixo_x.run();
+//  }
+//  /*desacelerando durante 150 passos*/
+//  
+//  motor_joao.move(800);
+//  while (motor_joao.distanceToGo() > 700){
+//    motor_joao.run();
+//  }
+//  /*100 passos a velocidade 100*/
+//  
+//  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 2);
+//  motor_eixo_x.move(PASSOS_PARA_A_PROXIMA_LETRA);
+//  while (motor_joao.distanceToGo() > 550){
+//    motor_joao.run();
+//    motor_eixo_x.run();
+//  }
+//  /*acelerando durante 150 passos*/
+//  
+//  motor_joao.setMaxSpeed(MOTOR_JOAO_VELOCIDADE / 4);
+//  while (motor_joao.distanceToGo() > 400){
+//    motor_joao.run();
+//    motor_eixo_x.run();
+//  }
+//  /*desacelerando durante 150 passos*/
 }
